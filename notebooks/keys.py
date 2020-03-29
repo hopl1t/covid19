7997c909e39643989141c5fc8e798c091aae845f
Nir Weingarten <nirzvi89@gmail.com>
2020-03-29 12:58:23 +0300

    # Added hopkins weather notebook # Added hopkins git repo # Updated hopkins dataset file # Added growth factor notebook template

--- notebooks/keys.py
+++ notebooks/keys.py
@@ -1,3 +1,12 @@
 app_id = ""
-client_id = "vv2IYjrS6kv2D52249jl9"
-client_secret = "ZbwMRSbTTYKsqBsQRkYyoquHjq2Enr1gkaXHxC33" 
+
+# new keys:
+# username: ZoharYechini
+# mail: nir.weingarten@post.idc.ac.il
+client_id = "tXyXXyb3Yf492X1asICz5"
+client_secret = "WJi1enKSDGlhO8Nt4Ze00C1F6LEdMpl2O54JIwaM"
+
+
+# Old keys:
+# client_id = "vv2IYjrS6kv2D52249jl9"
+# client_secret = "ZbwMRSbTTYKsqBsQRkYyoquHjq2Enr1gkaXHxC33" 
